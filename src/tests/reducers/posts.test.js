39c465470220995e9d4bcd94940bec1c66b278a6
Glapson <glapsable@gmail.com>
2019-02-25 08:35:48 +0100

    add postsReducer tests

---
+++ src/tests/reducers/posts.test.js
@@ -0,0 +1,44 @@
+import postsReducer from '../../reducers/posts';
+import posts from '../fixtures/posts';
+
+test('should add new post to posts state', () => {
+  const action = {
+    type: 'ADD_POST',
+    post: posts[0],
+  };
+  const state = postsReducer(undefined, action);
+  expect(state).toEqual([posts[0]]);
+});
+
+test('should edit post by id from posts array', () => {
+  const updates = {
+    title: 'updatedTitle',
+    description: 'updatedDescription',
+  };
+  const action = {
+    type: 'EDIT_POST',
+    id: posts[0].id,
+    updates,
+  };
+  const state = postsReducer(posts, action);
+  expect(state).toEqual([
+    {
+      ...posts[0],
+      ...updates,
+    },
+    posts[1],
+    posts[2],
+  ]);
+});
+
+test('should remove post by id from state', () => {
+  const action = {
+    type: 'REMOVE_POST',
+    id: posts[0].id,
+  };
+  const state = postsReducer(posts, action);
+  expect(state).toEqual([
+    posts[1],
+    posts[2],
+  ]);
+});
